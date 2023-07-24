4900ec91b71c11f28e4fd9e8eb67e9f7fa5e76b9
Hasin Hayder <countdraculla@gmail.com>
2023-07-24 21:50:34 +0600

    API

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,83 @@
 <script setup>
 import { ref, reactive } from 'vue'
 const joke = reactive({
-  value: 'Chuck Norris can do a wheelie on a unicycle.'
+  content: 'Chuck Norris can do a wheelie on a unicycle.'
 })
 
-function getANewJoke() {
+const jokex = ref('Chuck Norris can do a wheelie on a unicycle.')
+
+function getAJoke() {
   fetch('https://api.chucknorris.io/jokes/random')
     .then(response => response.json())
-    .then(data => joke.value = data.value)
+    .then(data => {
+      joke.content = data.value
+      // jokex.value = data.value
+    })
 }
 
-const countries = reactive([])
+const countries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Bhutan", capital: "Thimpu" },
+])
 
-function getCountries() {
+
+function getAllCountries (list = 'All') {
+  countries.length=0
   fetch('https://restcountries.com/v3.1/all?fields=name,capital')
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-      // for (const index in data) {
-      //   countries.push({ name: data[index].name.common, capital: data[index].capital[0] })
-      // }
-      data.filter(country=>country.name.common.startsWith('B')).forEach(country => {
-        countries.push({ name: country.name.common, capital: country.capital[0] })
-      })
-    }
-    )
+      if ('All' == list) {
+        data.forEach(country => {
+          countries.push({
+            name: country.name.common,
+            capital: country.capital[0]
+          })
+        })
+      }else{
+        data.filter(c=>c.name.common.startsWith(list)).forEach(country => {
+          countries.push({
+            name: country.name.common,
+            capital: country.capital[0]
+          })
+        })
+      }
+    })
 }
+
 </script>
 
 <template>
-  <section class="flex flex-col items-center w-[500px]">
+  <section class="flex flex-col items-center w-[500px] hidden">
     <p>
-      {{ joke.value }}
+      {{ joke.content }}
     </p>
-    <button @click="getANewJoke()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5" @click="getAJoke()">
       Get a new joke
     </button>
-    <div class="mt-5">
-      <ul name="countries" id="countries">
-        <li v-for="country in countries" :key="country.name" :value="country.name">
-          {{ country.name }}  = {{ country.capital }}
-        </li>
-      </ul>
-      <button @click="getCountries()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5">
-        Get Countries
-      </button>
-    </div>
+
+  </section>
+
+  <section class="container mx-auto">
+    <h1 class="text-2xl">Country List</h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5" @click="getAllCountries()">
+      List of Countries
+    </button>
+    <button class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" @click="getAllCountries('A')">
+      A
+    </button>
+    <button class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  mt-5" @click="getAllCountries('B')">
+      B
+    </button>
+    <button class="w-10 ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" @click="getAllCountries('C')">
+      C
+    </button>
+    <br>
+ 
+    <ul class="mt-10">
+      <li v-for="country in countries" :key="country.name">
+        {{ country.name }} = {{ country.capital }}
+      </li>
+    </ul>
   </section>
 </template>
 
